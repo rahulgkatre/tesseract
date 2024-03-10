e64b15a11bf23b4f9e51d11e4bd255f7e3af189f
Rahul Katre <38406805+rahulgkatre@users.noreply.github.com>
2024-03-10 17:16:39 -0400

    Finished loop serde with passing tests

--- src/Schedule.zig
+++ src/Schedule.zig
@@ -38,81 +38,13 @@ pub const Loop = struct {
     ptr: usize,
     body: std.MultiArrayList(ScheduleItem),
 
-    const JsonCompatScheduleItem = union(ScheduleItemEnum) {
-        loop: *JsonCompatLoop,
-        statement: *Statement,
-    };
-    const JsonCompatLoop = struct {
-        dim: u8,
-        reduce: bool,
-        ptr: usize,
-        body: []JsonCompatScheduleItem,
-    };
-
-    fn toJsonCompatLoop(self: *const Loop) !*JsonCompatLoop {
-        const body_as_slice = try allocator.alloc(JsonCompatScheduleItem, self.body.len);
-        for (self.body.items(.tags), self.body.items(.data), 0..) |tag, data, i| {
-            switch (tag) {
-                .loop => body_as_slice[i] = @unionInit(JsonCompatScheduleItem, @tagName(ScheduleItemEnum.loop), try toJsonCompatLoop(data.loop)),
-                inline else => |active| body_as_slice[i] = @unionInit(
-                    JsonCompatScheduleItem,
-                    @tagName(active),
-                    @field(data, @tagName(active)),
-                ),
-            }
-        }
-
-        const compat_loop = try allocator.create(JsonCompatLoop);
-        compat_loop.* = .{
-            .dim = self.dim,
-            .reduce = self.reduce,
-            .ptr = self.ptr,
-            .body = body_as_slice,
-        };
-        return compat_loop;
-    }
-
-    pub fn jsonStringify(self: *const Loop, write_stream: anytype) !void {
-        const compat_loop = try toJsonCompatLoop(self);
-        defer {
-            for (compat_loop.body) |item| {
-                switch (item) {
-                    inline else => |it| allocator.destroy(it),
-                }
-            }
-            allocator.free(compat_loop.body);
-            allocator.destroy(compat_loop);
-        }
-        try write_stream.write(compat_loop);
-    }
-
-    fn fromJsonCompatLoop(parsed_loop: *const JsonCompatLoop) !*Loop {
-        const loop = try arena.allocator().create(Loop);
-        loop.dim = parsed_loop.dim;
-        loop.reduce = parsed_loop.reduce;
-        loop.ptr = parsed_loop.ptr;
-        loop.body = .{};
-        try loop.body.ensureTotalCapacity(arena.allocator(), parsed_loop.body.len);
-        for (parsed_loop.body) |item| {
-            const converted_item = switch (item) {
-                .loop => |inner_loop| @unionInit(ScheduleItem, @tagName(ScheduleItemEnum.loop), try fromJsonCompatLoop(inner_loop)),
-                .statement => |inner_stmt| @unionInit(ScheduleItem, @tagName(ScheduleItemEnum.statement), inner_stmt),
-            };
-            loop.body.appendAssumeCapacity(converted_item);
-        }
-        return loop;
-    }
-
-    pub fn jsonParse(parse_allocator: std.mem.Allocator, source: *std.json.Scanner, options: std.json.ParseOptions) !Loop {
-        const parsed = try std.json.parseFromTokenSource(JsonCompatLoop, parse_allocator, source, options);
-        defer parsed.deinit();
-        return (try fromJsonCompatLoop(&parsed.value)).*;
-    }
-
     fn node(self: *const Loop) *Graph.TensorNode {
         return Graph.TensorNode.get(self.ptr);
     }
 
+    /// Calculate an ordinal value used for sorting a loop nest
+    /// Reduce loops are given a larger number so that they are the innermost loops
+    /// Original nesting order is preserved as loops are ordered by tensor dimension
     fn ordinal(self: *const Loop) u16 {
         const ndims = self.node().tensor.ndims;
         if (self.reduce) {
@@ -122,6 +54,7 @@ pub const Loop = struct {
         }
     }
 
+    /// Compare 2 loops, used for loop nest sorting
     fn loopCompare(_: void, lhs_loop: *const Loop, rhs_loop: *const Loop) bool {
         return lhs_loop.ordinal() < rhs_loop.ordinal();
     }
@@ -159,6 +92,93 @@ pub const Loop = struct {
         defer parsed.deinit();
         std.debug.print("{any}\n", .{parsed.value});
         return outermost_loop;
+    }
+
+    /// MultiArrayList does not automatically stringify so the default jsonStringify function
+    /// does not work. To make loops JSON compatible, the body is replaced with a slice of unions.
+    /// Deinit functions are provided as JsonCompatibleLoops are created with an allocator.
+    const JsonComptatibleItem = union(ScheduleItemEnum) {
+        loop: *JsonCompatibleLoop,
+        statement: *Statement,
+
+        fn deinit(self: *JsonComptatibleItem) void {
+            switch (self.*) {
+                .loop => |loop| loop.deinit(),
+                else => {},
+            }
+            allocator.destroy(self);
+        }
+    };
+    const JsonCompatibleLoop = struct {
+        dim: u8,
+        reduce: bool,
+        ptr: usize,
+        body: []JsonComptatibleItem,
+
+        fn deinit(self: *JsonCompatibleLoop) void {
+            for (self.body) |*item| {
+                item.deinit();
+            }
+            allocator.destroy(self);
+        }
+    };
+
+    fn toJsonCompatible(self: *const Loop) !*JsonCompatibleLoop {
+        const body_as_slice = try allocator.alloc(JsonComptatibleItem, self.body.len);
+        for (self.body.items(.tags), self.body.items(.data), 0..) |tag, data, i| {
+            switch (tag) {
+                .loop => body_as_slice[i] = @unionInit(JsonComptatibleItem, @tagName(ScheduleItemEnum.loop), try toJsonCompatible(data.loop)),
+                inline else => |active| body_as_slice[i] = @unionInit(
+                    JsonComptatibleItem,
+                    @tagName(active),
+                    @field(data, @tagName(active)),
+                ),
+            }
+        }
+
+        const compatible_loop = try allocator.create(JsonCompatibleLoop);
+        compatible_loop.* = .{
+            .dim = self.dim,
+            .reduce = self.reduce,
+            .ptr = self.ptr,
+            .body = body_as_slice,
+        };
+        return compatible_loop;
+    }
+
+    /// Custom jsonStringify implementation that converts a Loop to a JsonCompatibleLoop
+    /// and generates the Json string from the new loop
+    pub fn jsonStringify(self: *const Loop, write_stream: anytype) !void {
+        const compatible_loop = try toJsonCompatible(self);
+        defer compatible_loop.deinit();
+        try write_stream.write(compatible_loop);
+    }
+
+    fn fromJsonCompatible(parsed_loop: *const JsonCompatibleLoop) !*Loop {
+        // Use the arena allocator because the parsed loop will join the other loops of the schedule
+        var loop: *Loop = try arena.allocator().create(Loop);
+        loop.dim = parsed_loop.dim;
+        loop.reduce = parsed_loop.reduce;
+        loop.ptr = parsed_loop.ptr;
+        loop.body = .{};
+        try loop.body.ensureTotalCapacity(arena.allocator(), parsed_loop.body.len);
+        for (parsed_loop.body) |item| {
+            const converted_item = switch (item) {
+                .loop => |inner_loop| @unionInit(ScheduleItem, @tagName(ScheduleItemEnum.loop), try fromJsonCompatible(inner_loop)),
+                .statement => |inner_stmt| @unionInit(ScheduleItem, @tagName(ScheduleItemEnum.statement), inner_stmt),
+            };
+            loop.body.appendAssumeCapacity(converted_item);
+        }
+        return loop;
+    }
+
+    /// Custom jsonStringify implementation that converts a parsed JsonCompatibleLoop
+    /// into a regular loop
+    pub fn jsonParse(parse_allocator: std.mem.Allocator, source: *std.json.Scanner, options: std.json.ParseOptions) !Loop {
+        const parsed = try std.json.parseFromTokenSource(JsonCompatibleLoop, parse_allocator, source, options);
+        defer parsed.deinit();
+        const loop = try fromJsonCompatible(&parsed.value);
+        return loop.*;
     }
 };
 
@@ -209,18 +229,27 @@ pub fn create() void {
 }
 
 test "double nested loop deserialization" {
+    init(std.testing.allocator);
+    defer deinit();
     const str =
-        \\{"dim":0,"reduce":false,"ptr":18146776,"body":[{"loop":{"dim":1,"reduce":false,"ptr":18146776,"body":[]}}]}
+        \\{"dim":0,"reduce":false,"ptr":0,"body":[{"loop":{"dim":1,"reduce":false,"ptr":0,"body":[]}}]}
     ;
     const parsed = try std.json.parseFromSlice(Loop, std.testing.allocator, str, .{});
     defer parsed.deinit();
-    std.debug.print("\n{any}\n", .{parsed.value});
+    const new_str = try std.json.stringifyAlloc(std.testing.allocator, parsed.value, .{});
+    defer std.testing.allocator.free(new_str);
+    std.debug.assert(std.mem.eql(u8, str, new_str));
 }
 
 test "triple nested loop deserialization" {
+    init(std.testing.allocator);
+    defer deinit();
     const str =
-        \\{"dim":0,"reduce":false,"ptr":18146776,"body":[{"loop":{"dim":1,"reduce":false,"ptr":18146776,"body":[{"loop":{"dim":2,"reduce":false,"ptr":18146776,"body":[]}}]}}]}
+        \\{"dim":0,"reduce":false,"ptr":0,"body":[{"loop":{"dim":1,"reduce":false,"ptr":0,"body":[{"loop":{"dim":2,"reduce":false,"ptr":0,"body":[]}}]}}]}
     ;
-    _ = str;
-    // std.debug.print("\n{any}\n", .{std.json.parseFromSlice(Loop, std.testing.allocator, str, .{})});
+    const parsed = try std.json.parseFromSlice(Loop, std.testing.allocator, str, .{});
+    defer parsed.deinit();
+    const new_str = try std.json.stringifyAlloc(std.testing.allocator, parsed.value, .{});
+    defer std.testing.allocator.free(new_str);
+    std.debug.assert(std.mem.eql(u8, str, new_str));
 }
